6b39311d4ccd3fbfa566b682a1ee77ce271d0859
Toffe Laptop <toffehelstad@gmail.com>
2022-04-30 16:00:26 +0200

    Feat: Added NavMesh

---
+++ Assets/Scripts/NavMesh Scripts/MoveTo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+    public Transform goal;
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        
+        agent.destination = goal.position;
+    }
+}
